f43b342cb6e7738524074afd32859dfc8f163119
Ezinw <112014367+Ezinw@users.noreply.github.com>
2023-08-29 07:38:30 -0300

    Add files via upload

---
+++ src/Implementation.cs
@@ -0,0 +1,12 @@
+﻿using MelonLoader;
+
+namespace QuickSave
+{
+    internal sealed class Implementation : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+
+        }
+    }
+}
---
+++ src/Patches.cs
@@ -0,0 +1,130 @@
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppParadoxNotion.Services;
+using Il2CppTLD.Gameplay;
+using UnityEngine;
+
+namespace QuickSave
+{
+
+    //Quicksaving...
+    [HarmonyPatch(typeof(GameManager), "Update")]
+    internal class QuickSave
+    {
+        private static readonly KeyCode saveKey = KeyCode.F5;
+
+        static void Postfix(GameManager __instance)
+        {
+            if (Input.GetKeyDown(saveKey))
+            {
+                GameManager.SaveGameAndDisplayHUDMessage();
+            }
+        }
+    }
+
+    //Quickloading...
+    [HarmonyPatch(typeof(GameManager), "Update")]
+    internal class QuickLoad
+    {
+        private static readonly KeyCode loadKey = KeyCode.F6;
+
+        static void Postfix(GameManager __instance)
+        {
+            if (Input.GetKeyDown(loadKey))
+            {
+                if (GameManager.IsMainMenuActive())
+                    return;
+
+                if (GameManager.GetExperienceModeManagerComponent().InCustomMode())
+                {
+                    SaveSlotInfo quickSaveSlot = SaveGameSystem.GetNewestSaveSlotForActiveGame();
+
+                    if (quickSaveSlot != null)
+                    {
+                        GameManager.LoadSaveGameSlot(quickSaveSlot);
+                    }
+                }
+                if (GameManager.GetExperienceModeManagerComponent().IsChallengeActive())
+                {
+                    SaveSlotInfo quickSaveSlot = SaveGameSystem.GetNewestSaveSlotForActiveGame();
+
+                    if (quickSaveSlot != null)
+                    {
+                        GameManager.LoadSaveGameSlot(quickSaveSlot);
+                    }
+                }
+                if (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Pilgrim)
+                {
+                    SaveSlotInfo quickSaveSlot = SaveGameSystem.GetNewestSaveSlotForActiveGame();
+
+                    if (quickSaveSlot != null)
+                    {
+                        GameManager.LoadSaveGameSlot(quickSaveSlot);
+                    }
+                }
+                if (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Voyageur)
+                {
+                    SaveSlotInfo quickSaveSlot = SaveGameSystem.GetNewestSaveSlotForActiveGame();
+
+                    if (quickSaveSlot != null)
+                    {
+                        GameManager.LoadSaveGameSlot(quickSaveSlot);
+                    }
+                }
+                if (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Stalker)
+                {
+                    SaveSlotInfo quickSaveSlot = SaveGameSystem.GetNewestSaveSlotForActiveGame();
+
+                    if (quickSaveSlot != null)
+                    {
+                        GameManager.LoadSaveGameSlot(quickSaveSlot);
+                    }
+                }
+                if (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Interloper)
+                {
+                    SaveSlotInfo quickSaveSlot = SaveGameSystem.GetNewestSaveSlotForActiveGame();
+
+                    if (quickSaveSlot != null)
+                    {
+                        GameManager.LoadSaveGameSlot(quickSaveSlot);
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    
+    [HarmonyPatch(typeof(GameAudioManager), "PlayGUIError")]
+    class GameAudioManager_PlayGUIError_Patch
+    {
+        static bool Prefix(GameAudioManager __instance)
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                AudioSource audioSource = __instance.gameObject.GetComponent<AudioSource>();
+
+                if (audioSource.clip != null && audioSource.clip.name == "PlayGUIError")
+                {
+                    audioSource.Stop();
+
+                    return false;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                AudioSource audioSource = __instance.gameObject.GetComponent<AudioSource>();
+
+                if (audioSource.clip != null && audioSource.clip.name == "PlayGUIError")
+                {
+                    audioSource.Stop();
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
