a105481f51f5a66015dea356e60117f4e2929cfa
Ezinw <112014367+Ezinw@users.noreply.github.com>
2024-12-11 07:22:05 -0400

    Add files via upload

--- src/PanelCheck.cs
+++ src/PanelCheck.cs
@@ -5,68 +5,82 @@ namespace QuickSave
 {
     public static class PanelCheck
     {
+        private static readonly List<Il2CppSystem.Type> panelTypes = new()
+          {
+               Il2CppType.Of<Panel_ActionPicker>(),
+               Il2CppType.Of<Panel_Actions>(),
+               Il2CppType.Of<Panel_ActionsRadial>(),
+               Il2CppType.Of<Panel_Affliction>(),
+               Il2CppType.Of<Panel_Badges>(),
+               Il2CppType.Of<Panel_Base>(),
+               Il2CppType.Of<Panel_BedRollSelect>(),
+               Il2CppType.Of<Panel_BodyHarvest>(),
+               Il2CppType.Of<Panel_BodyHarvest_MenuItem>(),
+               Il2CppType.Of<Panel_BreakDown>(),
+               Il2CppType.Of<Panel_Camera>(),
+               Il2CppType.Of<Panel_CanOpening>(),
+               Il2CppType.Of<Panel_Clothing>(),
+               Il2CppType.Of<Panel_Confirmation>(),
+               Il2CppType.Of<Panel_Container>(),
+               Il2CppType.Of<Panel_Cooking>(),
+               Il2CppType.Of<Panel_CookWater>(),
+               Il2CppType.Of<Panel_Crafting>(),
+               Il2CppType.Of<Panel_Credits>(),
+               Il2CppType.Of<Panel_CuringStorage>(),
+               Il2CppType.Of<Panel_Diagnosis>(),
+               Il2CppType.Of<Panel_ExchangeBox>(),
+               Il2CppType.Of<Panel_Extras>(),
+               Il2CppType.Of<Panel_FeedFire>(),
+               Il2CppType.Of<Panel_FireStart>(),
+               Il2CppType.Of<Panel_FirstAid>(),
+               Il2CppType.Of<Panel_GearSelect>(),
+               Il2CppType.Of<Panel_Harvest>(),
+               Il2CppType.Of<Panel_Help>(),
+               Il2CppType.Of<Panel_IceFishing>(),
+               Il2CppType.Of<Panel_IceFishingHoleClear>(),
+               Il2CppType.Of<Panel_InsulatedFlask>(),
+               Il2CppType.Of<Panel_Inventory>(),
+               Il2CppType.Of<Panel_Inventory_Examine>(),
+               Il2CppType.Of<Panel_Inventory_Examine_MenuItem>(),
+               Il2CppType.Of<Panel_LifeAfterDeath>(),
+               Il2CppType.Of<Panel_Log>(),
+               Il2CppType.Of<Panel_MainMenu>(),
+               Il2CppType.Of<Panel_Map>(),
+               Il2CppType.Of<Panel_Milling>(),
+               Il2CppType.Of<Panel_MissionsStory>(),
+               Il2CppType.Of<Panel_OptionsMenu>(),
+               Il2CppType.Of<Panel_PauseMenu>(),
+               Il2CppType.Of<Panel_PickUnits>(),
+               Il2CppType.Of<Panel_PickWater>(),
+               Il2CppType.Of<Panel_RecipeBook>(),
+               Il2CppType.Of<Panel_Repair>(),
+               Il2CppType.Of<Panel_Rest>(),
+               Il2CppType.Of<Panel_SafeCracking>(),
+               Il2CppType.Of<Panel_SelectWorldMap>(),
+               Il2CppType.Of<Panel_SnowShelterBuild>(),
+               Il2CppType.Of<Panel_SnowShelterInteract>(),
+               Il2CppType.Of<Panel_SprayPaint>(),
+               Il2CppType.Of<Panel_Stats_DetailedEntry>(),
+               Il2CppType.Of<Panel_Stats_SummaryEntry>(),
+               Il2CppType.Of<Panel_Story_MissionJumpItem>(),
+               Il2CppType.Of<Panel_TorchLight>(),
+               Il2CppType.Of<Panel_TradeSelection>(),
+               Il2CppType.Of<Panel_TutorialPopup>(),
+               Il2CppType.Of<Panel_WeaponPicker>(),
+          };
+
         public static bool IsPanelOpen()
         {
-            return InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_ActionPicker>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Actions>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_ActionsRadial>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Affliction>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Badges>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Base>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_BedRollSelect>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_BodyHarvest>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_BodyHarvest_MenuItem>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_BreakDown>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Camera>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_CanOpening>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Clothing>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Confirmation>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Container>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Cooking>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_CookWater>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Crafting>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Credits>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_CuringStorage>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Diagnosis>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_ExchangeBox>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Extras>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_FeedFire>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_FireStart>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_FirstAid>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_GearSelect>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Harvest>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Help>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_IceFishing>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_IceFishingHoleClear>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_InsulatedFlask>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Inventory>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Inventory_Examine>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Inventory_Examine_MenuItem>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_LifeAfterDeath>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Log>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_MainMenu>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Map>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Milling>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_MissionsStory>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_OptionsMenu>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_PauseMenu>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_PickUnits>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_PickWater>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_RecipeBook>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Repair>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Rest>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_SafeCracking>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_SelectWorldMap>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_SnowShelterBuild>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_SnowShelterInteract>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_SprayPaint>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Stats_DetailedEntry>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Stats_SummaryEntry>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_Story_MissionJumpItem>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_TorchLight>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_TradeSelection>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_TutorialPopup>()) ||
-                   InterfaceManager.IsPanelEnabled(Il2CppType.Of<Panel_WeaponPicker>());
+            foreach (var panelType in panelTypes)
+            {
+                if (InterfaceManager.IsPanelEnabled(panelType))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
         }
     }
 }
